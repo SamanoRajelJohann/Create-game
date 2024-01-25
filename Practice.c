da1e8b1ca0bfb2fa809bf9abc3f5679e28f214c8
SamanoRajelJohann <ragegamer90samanoa@gmail.com>
2024-01-25 18:12:06 +0800

    Creating file for practice

---
+++ Practice.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+struct Characters {
+    int HP;
+    int ATK;
+    int Mpwr;
+    int Shield;
+};
+
+struct Enemies {
+    int HP;
+    int ATK;
+    int Mpwr;
+    int Shield;
+};
+
+struct Gamenumbers {
+    int Healing;
+};
+
+struct Heroic_Weapons {
+    int Excalibur;
+    int Megalithic;
+    int Dawnspark;
+    int Moonblade;
+    int Gorehowl;
+}
